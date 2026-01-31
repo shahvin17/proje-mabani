5192539380e2b2ee02b5ac112191fcff23bc22a6
shervinconfig <shrveinba@gmail.com>
2026-01-31 17:48:55 +0330

    bebin oomade

---
+++ testtttttttttt.cpp
@@ -0,0 +1,15 @@
+//
+// Created by benyamin on 1/31/2026.
+//
+
+#include "testtttttttttt.h"
+
+namespace zaaaaaaaaaa {
+} // zaaaaaaaaaa
+#include "iostream"
+
+using namespace std;
+int main (){
+    cout<<"zaaaaaaadddddddddddaaaaaaaaaaaaaannnnnnnnnnnnnn";
+    return 0 ;
+}
---
+++ testtttttttttt.h
@@ -0,0 +1,16 @@
+//
+// Created by benyamin on 1/31/2026.
+//
+
+#ifndef PROJE_MABANI_TESTTTTTTTTTTT_H
+#define PROJE_MABANI_TESTTTTTTTTTTT_H
+
+namespace zaaaaaaaaaa {
+
+    class testtttttttttt {
+
+    };
+
+} // zaaaaaaaaaa
+
+#endif //PROJE_MABANI_TESTTTTTTTTTTT_H
