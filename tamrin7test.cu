4ceeff2520016bd969c2aa4aba8ab83c85dc4fb4
shervinconfig <shrveinba@gmail.com>
2026-02-02 21:12:01 +0330

    emtehan mikonim

---
+++ tamrin7test.cu
@@ -0,0 +1,5 @@
+//
+// Created by benyamin on 2/2/2026.
+//
+
+#include "tamrin7test.cuh"
---
+++ tamrin7test.cuh
@@ -0,0 +1,14 @@
+//
+// Created by benyamin on 2/2/2026.
+//
+
+#ifndef PROJE_MABANI_TAMRIN7TEST_CUH
+#define PROJE_MABANI_TAMRIN7TEST_CUH
+
+
+class tamrin7test {
+
+};
+
+
+#endif //PROJE_MABANI_TAMRIN7TEST_CUH
