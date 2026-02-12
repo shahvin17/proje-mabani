17fdd5d6e90085c668d66c2416fc67cbd4e100f6
shervinconfig <shrveinba@gmail.com>
2026-02-12 17:45:13 +0330

    d

---
+++ .idea/logic.cpp
@@ -0,0 +1,68 @@
+//
+// Created by benyamin on 2/12/2026.
+//
+
+#include "logic.h"
+#include <iostream>
+#include "math.h"
+double DEG_TO_RAD (double a ) {double c = (a* 3.14159) /( 180.0);
+    return c ;}
+typedef struct {
+    char name[50];
+    double x;
+    double y;
+    double direction;
+    int size;
+    bool isVisible;
+}
+        Sprite;
+void moveSteps(Sprite* spr, double steps) {
+
+
+    spr->x += steps * sin(DEG_TO_RAD(spr->direction));
+    spr->y -= steps * cos(DEG_TO_RAD(spr->direction));
+}
+
+void normalizeDirection(Sprite* spr) {
+    spr->direction = fmod(spr->direction, 360.0);
+    if (spr->direction < 0) spr->direction += 360.0;
+}
+
+void turnRight(Sprite* spr, double degrees) {
+    spr->direction += degrees;
+    normalizeDirection(spr);
+}
+
+void turnLeft(Sprite* spr, double degrees) {
+    spr->direction -= degrees;
+    normalizeDirection(spr);
+}
+
+void goToXY(Sprite* spr, double newX, double newY) {
+    spr->x = newX;
+    spr->y = newY;
+}
+void changeX(Sprite* spr, double amount) {
+    spr->x += amount;
+}
+
+void setX(Sprite* spr, double newX) {
+    spr->x = newX;
+}
+
+void changeY(Sprite* spr, double amount) {
+    spr->y -= amount;
+}
+
+void pointInDirection(Sprite* spr, double newDirection) {
+    spr->direction = newDirection;
+    normalizeDirection(spr);
+}
+
+int main() {
+
+
+
+    return 0;
+
+}
--- tamrin7test.cu
+++
@@ -1,5 +0,0 @@
-//
-// Created by benyamin on 2/2/2026.
-//
-
-#include "tamrin7test.cuh"
