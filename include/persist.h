c131141c83188ed0ae4907aa7066c6daf210ef90
shervinconfig <shrveinba@gmail.com>
2026-02-18 19:24:58 +0330

    dobare32

--- include/persist.h
+++ include/persist.h
@@ -1,3 +1,25 @@
+#ifndef PERSIST_H
+#define PERSIST_H
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <vector>
+#include <string>
+struct Block {
+    int id;
+    string type;
+    vector<int> inputs;
+    int nextBlockId;
+    float x, y;
+};
+
+struct Sprite {
+    int id;
+    string name;
+    float x, y;
+    float direction;
+    bool visible;
+};
 
 struct Project {
     vector<Sprite> sprites;
