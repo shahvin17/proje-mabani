067a0693119db055fcf8a443f3245b2499a311da
shervinconfig <shrveinba@gmail.com>
2026-02-18 17:29:46 +0330

    ا

---
+++ include/logger.h
@@ -0,0 +1,14 @@
+
+#ifndef LOGGER_H
+#define LOGGER_H
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <string>
+void logInfo(const std::string& msg);
+void logWarning(const std::string& msg);
+void logError(const std::string& msg);
+logInfo("Project saved successfully.");
+logError("Failed to load file.");
+
+
