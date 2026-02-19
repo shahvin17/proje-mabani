36e1d9bf2e5a117c59cc722b4047d7469df7f32f
shervinconfig <shrveinba@gmail.com>
2026-02-19 23:19:13 +0330

    update event

--- src/events.cpp
+++ src/events.cpp
@@ -15,3 +15,14 @@ const std::vector<int>* event_get(const EventManager& em, EventType type, int co
     if (it == em.table.end()) return nullptr;
     return &it->second;
 }
+static int hash_message(const std::string& msg) {
+    return static_cast<int>(std::hash<std::string>{}(msg));
+}
+void event_bind_message(EventManager& em, const std::string& message, int startBlockId) {
+    int msgCode = hash_message(message);
+    event_bind(em, EVENT_ON_MESSAGE, msgCode, startBlockId);
+}
+const std::vector<int>* event_get_message(const EventManager& em, const std::string& message) {
+    int msgCode = hash_message(message);
+    return event_get(em, EVENT_ON_MESSAGE, msgCode);
+}
