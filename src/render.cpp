24333b033dcc71faca906d4c9b207881b0d2c8e3
Shadi-doroodgar <doroodgarshadi@gmail.com>
2026-02-20 02:44:09 +0330

    render, hit test

--- src/render.cpp
+++ src/render.cpp
@@ -44,3 +44,16 @@ void drawWindow(const string Title, const unsigned int x1, y1, x2, y2){
 
 void drag(){
 }
+
+#include "render.h"
+
+void renderRect(SDL_Renderer* renderer, const DragRect& r) {
+    SDL_Rect rect{r.x, r.y, r.w, r.h};
+    SDL_SetRenderDrawColor(renderer, 255, 80, 80, 255);
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+bool isInside(const DragRect& r, int mx, int my) {
+    return mx >= r.x && mx <= r.x + r.w &&
+           my >= r.y && my <= r.y + r.h;
+}
