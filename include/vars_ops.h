992abe25217af1a4d3cb81b2ceb9d067b7c18729
shervinconfig <shrveinba@gmail.com>
2026-02-20 01:00:56 +0330

    w

--- include/vars_ops.h
+++ include/vars_ops.h
@@ -41,4 +41,32 @@ const Value* var_get(const VarStore& vs, const std::string& name);
 
 const std::unordered_map<std::string, Value>& var_all(const VarStore& vs);
 
+
+Value op_add(const Value& a, const Value& b);
+Value op_sub(const Value& a, const Value& b);
+Value op_mul(const Value& a, const Value& b);
+Value op_div(const Value& a, const Value& b, bool& out_error);
+
+
+Value op_greater_than(const Value& a, const Value& b);
+Value op_less_than(const Value& a, const Value& b);
+Value op_equals(const Value& a, const Value& b);
+
+Value op_and(const Value& a, const Value& b);
+Value op_or(const Value& a, const Value& b);
+Value op_not(const Value& a);
+Value op_string_length(const Value& str);
+Value op_string_join(const Value& str1, const Value& str2);
+Value op_string_letter_at(const Value& str, const Value& index, bool& out_error);
+
+
+Value op_abs(const Value& a);
+Value op_sqrt(const Value& a, bool& out_error);
+Value op_floor(const Value& a);
+Value op_ceil(const Value& a);
+Value op_sin(const Value& a);
+Value op_cos(const Value& a);
+Value op_modulo(const Value& a, const Value& b, bool& out_error);
+Value op_xor(const Value& a, const Value& b);
+
 #endif
