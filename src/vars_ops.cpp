517db0fa55725b9609b5c2937ffd1ee7cc2082b8
shervinconfig <shrveinba@gmail.com>
2026-02-19 18:25:04 +0330

    ااااا

--- src/vars_ops.cpp
+++ src/vars_ops.cpp
@@ -7,7 +7,7 @@ bool var_define(VarStore& vs, const std::string& name, const Value& initial) {
     if (vs.vars.find(name) != vs.vars.end())
         return false; 
 
-    vs.vars[name] = initial; // تعریف (52)
+    vs.vars[name] = initial;
     return true;
 }
 
