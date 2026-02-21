9f69d7aa597b3409dce1815516818fb382d65807
Shahvin17 <shrveinba@gmail.com>
2026-02-21 17:17:50 +0330

    yy

---
+++ include/logger.h
@@ -0,0 +1,20 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "core_types.h"
+#include <string>
+
+std::string get_system_help_text();
+
+void safety_clamp_sprite(Sprite& spr, float screen_width, float screen_height);
+
+struct WatchdogState {
+    int current_frame_instructions = 0;
+    int max_allowed_instructions = 5000;
+};
+
+void safety_reset_watchdog(WatchdogState& wd);
+
+bool safety_check_watchdog(WatchdogState& wd, std::string& out_error_message);
+
+#endif
